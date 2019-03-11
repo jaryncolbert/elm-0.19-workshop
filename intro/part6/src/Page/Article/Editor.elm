e8af62723806d6d869d5157764b93db6f6cc7a32
Jaryn Colbert <jaryn.colbert@gmail.com>
2019-03-11 19:09:49 -0400

    Part 6 - Return default avatar. Filter empty tags

--- intro/part6/src/Page/Article/Editor.elm
+++ intro/part6/src/Page/Article/Editor.elm
@@ -572,6 +572,7 @@ toTagList tagString =
     -}
     String.split " " tagString
         |> List.map String.trim
+        |> List.filter (\str -> not (String.isEmpty str ))
 
 
 edit : Slug -> TrimmedForm -> Cred -> Http.Request (Article Full)
